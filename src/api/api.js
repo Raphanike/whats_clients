6ad1a10d030daa698e1d6d6ed45a666ced39dff4
Raphinha <raphinha.maciel@outlook.com>
2025-05-19 19:16:44 -0300

    "dfgvfdg"

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://whats-back-end-7.onrender.com/api' // correto!
+    baseURL: 'https://whats-back-end-7.onrender.com/api'
 })
 
 export default api
