38ccf1903c742daac20101637d75bc15b30435df
Raphinha <raphinha.maciel@outlook.com>
2025-05-20 20:12:19 -0300

    sds

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import LoginForm from '../components/LoginForm'
 export default function Login({ onLogin }) {
     return (
         <div className='login'>
-        <h1>WhatsApp<img src="./img/whats.png" alt="" /></h1>
+        <h1><img src="./src/img/whats.png" alt="" /></h1>
         <LoginForm onLogin={onLogin} />
         </div>
     )
