fa567d98ef30b0fb88feef79acccf14f255a727e
RAPHAEL CAVALCANTE <139821771+Raphanike@users.noreply.github.com>
2025-09-30 13:44:41 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Login from './pages/Login'
 import Chat from './pages/Chat'
 
 function App() {
   const [user, setUser] = useState(null)
 
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'Notification' in window) {
+      if (Notification.permission !== 'granted') {
+        Notification.requestPermission()
+      }
+    }
+  }, [])
 
-  return (
-    <div>
-      {user ? <Chat user={user} /> : <Login onLogin={setUser} />}
-    </div>
-  )
+  return <div>{user ? <Chat user={user} /> : <Login onLogin={setUser} />}</div>
 }
 
 export default App
